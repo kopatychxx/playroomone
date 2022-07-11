9f349c04f4245a3696f764d879cfb198b268f838
kopatychxx <evgenijsalo88@gmail.com>
2022-07-11 15:42:47 +0200

    Add_info_to_Program_Page

--- src/views/ProgramPage.vue
+++ src/views/ProgramPage.vue
@@ -15,17 +15,53 @@
       color: #000000;"
         >Садок Playroom англомовний простір де 80% всього часу діти спілкуються з дорослими англійською мовою</p>
       </div>
-      <img src="../assets/main.png" alt="">
+      <img src="../assets/program.svg" alt="" width="100%">
+    </div>
+    <div class="programPage__rows"           style="text-align: center;margin-top:50px;row-gap: 20px;display: flex;flex-direction: column">
+      <div class="programPage__rows-row"
+           v-for="(language, i) in languages"
+           :key="i">
+        <div class="alltext" style="width:90%"><span>{{language.title}}</span>{{ language.text }}</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ProgramPage'
+  name: 'ProgramPage',
+  data: () => ({
+    languages: [
+      {
+        title: 'Ми працюємо за власною програмою навчання, ',
+        text: 'виховання та розвитку дітей, в основу якої покладена американська програма розвитку дітей дошкільного віку “Creative Curriculum”, але ми також використовуємо елементи сучасних  і ефективних методичних систем раннього розвитку та виховання відомих світових авторів'
+      },
+      {
+        title: '“Creative Curriculum”'
+      },
+      {
+        title: 'Навчання в Playroom',
+        text: ' - це комплекс захопливих уроків, що проходять в ігровій формі. Серед предметів, які ми вивчаємо повноцінна підготовка до школи : математика, навколишній світ, розвиток мовлення. Фізична підготовка поєднана з танцями та театральним дійством. \n' +
+          'У нас діти занурюються в англомовний світ гри, навчальних пісень та віршиків з рухливими вправами та можливістю виразити себе у мистецтві. Також двічі на тиждень маємо заняття з французької мови. Наша мета – надати якісні та ефективні знання таким чином, щоб навчальний процес не був нав’язливим для дитини, приносив користь та задоволення\n'
+      }
+    ]
+  })
 }
 </script>
 
 <style scoped>
-
+.cquared {
+  width: 20px;
+  height: 3px;
+  background: #E83542;
+  margin: 0 20px;
+}
+.programPage__rows-row{
+  display: flex;
+  align-items: center;
+}
+.alltext span{
+  font-weight: 500;
+  color: #E83542;
+}
 </style>
